3bf1a39e93775f7cbcc2bfbc6231f77f2abdb7eb
ThgCH <manhduc889@gmail.com>
2024-03-04 18:24:00 +0700

    update

--- User/connect.php
+++ User/connect.php
@@ -1,10 +1,9 @@
 <?php 
 $servername="localhost";
 $username="root";
-$password="123456789";
-$port="3306";
+$password="";
 $database="music_web";
-$conn = new mysqli($servername,$username,$password,$database,$port);
+$conn = new mysqli($servername,$username,$password,$database);
 if ($conn->connect_error){
 	die("Lỗi kết nối với CSDL");
 }
