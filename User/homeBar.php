83fc90efec0da167a76a90b4d503c2f2ac043e72
TeitoKlein2k3 <phanthanhnam200@gmail.com>
2024-03-05 00:12:06 +0700

    dcutknaolamuserloiloiken

--- User/homeBar.php
+++ User/homeBar.php
@@ -1,7 +1,7 @@
 <div class ="homeBar"> 
 <link rel="stylesheet" type="text/css" href="style.css">
     <?php
-    $imageUrl = "../images/logo/logo.png"; // replace with the actual path or URL of your image
+    $imageUrl = "../images/logo/logo.png"; 
     ?>
 
     <img src="<?php echo $imageUrl; ?>" alt="Your Image">
@@ -58,7 +58,7 @@
 
         <div class="has-subnav">
             <a href="index.php?sort=avatar">
-                <div class="Avatar" style="display: flex;flex-direction: row;justify-content: space-between;align-items: center;padding: 16px 24px;gap: 16px;width: 275px;height: 60px;background: #B6B5E3;border-radius: 12px;flex: none;order: 1;align-self: stretch;flex-grow: 0;top: 220px; position:relative;">
+                <div class="Avatar" style="display: flex;flex-direction: row;justify-content: space-between;align-items: center;padding: 16px 24px;gap: 16px;width: 275px;height: 60px;background: #B6B5E3;border-radius: 12px;flex: none;order: 1;align-self: stretch;flex-grow: 0;top: 20px; position:relative;">
                 <img src="../images/logo/Avatar.png">
                 <span class="nav-text" style="width: 770px;height: 18px;font-family: 'Roboto';font-style: normal;font-weight: 400;font-size: 16px;line-height: 18px;color: #000000;flex: none;order: 1;flex-grow: 0;
 ">
@@ -69,5 +69,20 @@
             </a><br>
 
     </div>
-    
+    <style>
+         a {
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+            .homeBar {
+                border-radius:10px;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            }
+            .HOME:focus{
+                 background-color: #EEEEEE;
+            }
+    </style>
     </div>
--- User/login.php
+++ User/login.php
@@ -8,22 +8,32 @@
 </head>
 
 <body>
+   <div class="login">
     <form action="login_action.php" method="post">
-        <input type="text" placeholder="Tên đăng nhập" name="username" id="username_txt">
-        <input type="password" placeholder="Mật khẩu" name="password" id="password_txt">
-        <?php
-        // Display error message if redirected with an error parameter
-        if (isset($_GET['error']) && $_GET['error'] == 1) {
-            echo '<p style="color: red;">Sai tên đăng nhập hoặc mật khẩu. Vui lòng thử lại.</p>';
-        }
-        else if(isset($_GET['error']) && $_GET['error'] == 7) {
-            echo '<p style="color: red;">Đăng ký tạo tài khoản mới thành công</p>';
-        }
-        ?>
-        <button type="submit">Đăng nhập</button>
-    </form>
+            <input type="text" placeholder="Tên đăng nhập" name="username" id="username_txt">
+            <input type="password" placeholder="Mật khẩu" name="password" id="password_txt">
+            <?php
+            // Display error message if redirected with an error parameter
+            if (isset($_GET['error']) && $_GET['error'] == 1) {
+                echo '<p style="color: red;">Sai tên đăng nhập hoặc mật khẩu. Vui lòng thử lại.</p>';
+            }
+            else if(isset($_GET['error']) && $_GET['error'] == 7) {
+                echo '<p style="color: red;">Đăng ký tạo tài khoản mới thành công</p>';
+            }
+            ?>
+            <button type="submit">Đăng nhập</button>
+        </form>
+   </div>
 
 
 </body>
+<style>
+    .login{
+        background-image: url('../images/logo/bglogin.png');    
+        background-size: cover;
+        background-position: center;
+        position: relative;
+    }
+</style>
 
 </html>
